6bae25d9566d23a76ad0761be4cff747e6266128
jackie <99sharmatushar@gmail.com>
2020-10-20 17:33:03 +0530

    Add procfile and update ports

--- server.js
+++ server.js
@@ -51,7 +51,12 @@ app.post("/charge",function(req,res){
   })
 })
 
-app.listen(3000)
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port)
 {
   console.log("server up and running on port 3000")
 }
